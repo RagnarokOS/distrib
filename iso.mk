58522afc1d47d536ed262df77fc757ab62b347ea
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2023-06-13 13:26:28 -0400

    Fix

--- iso.mk
+++ iso.mk
@@ -5,7 +5,7 @@ ISO_MODE	= live
 HDD_LABEL	= RAGNAROK
 BOOTPARAMS	= boot=live live-config.hooks=filesystem live-config.locales=en_CA.UTF-8 \
 		  live-config.timezone=Canada/Eastern live-config.hostname=ragnarok \
-		  live-config.noautologin live-config.sysv-rc=opensmtpd slab_nomerge \
+		  live-config.noautologin live-config.sysv-rc=openntpd slab_nomerge \
 		  init_on_alloc=1 init_on_free=1 page_alloc.shuffle=1 pti=on \
 		  randomize_kstack_offset=on vsyscall=none debugfs=off \
 		  lockdown=confidentiality
