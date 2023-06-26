fad1884d83300d77c0eca92b515358f89409bdfc
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2023-06-26 13:34:21 -0400

    add console=tty0

--- iso/live.mk
+++ iso/live.mk
@@ -9,7 +9,7 @@ X11_BOOTPARAMS	= boot=live live-config.hooks=filesystem live-config.locales=en_C
 		  init_on_alloc=1 init_on_free=1 page_alloc.shuffle=1 pti=on \
 		  randomize_kstack_offset=on vsyscall=none debugfs=off \
 		  lockdown=confidentiality
-CLI_BOOTPARAMS	= boot=live live-getty console=ttyS0,115200n8 live-config.hooks=filesystem \
+CLI_BOOTPARAMS	= boot=live live-getty console=tty0 console=ttyS0,115200n8 live-config.hooks=filesystem \
 		  live-config.locales=en_CA.UTF-8 live-config.timezone=Canada/Eastern \
 		  live-config.hostname=ragnarok live-config.noautologin live-config.sysv-rc=openntpd \
 		  slab_nomerge init_on_alloc=1 init_on_free=1 page_alloc.shuffle=1 pti=on \
