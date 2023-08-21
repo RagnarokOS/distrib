6947621d31583a1c35aba4963b72dfcf5fe0b803
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2023-08-21 18:59:11 -0400

    This is better

--- live/config/includes.chroot_after_packages/etc/skel/.local/bin/ffinstall.sh
+++ live/config/includes.chroot_after_packages/etc/skel/.local/bin/ffinstall.sh
@@ -11,7 +11,7 @@ VERSION="116.0.3"
 LOC=${LOCALE:-en-CA}
 NAME="firefox-${VERSION}.tar.bz2"
 URL="https://download-installer.cdn.mozilla.net/pub/firefox/releases/${VERSION}/linux-x86_64/${LOC}/${NAME}"
-USERJS=${USERJS:-yes}
+USERSETUP=${USERSETUP:-yes}
 
 [[ $(id -u) == 0 ]] && printf '%s\n' "Do not run this script as root." && exit 1
 
@@ -25,11 +25,18 @@ create_profile() {
 
 	_pname=$(find .mozilla/firefox/ -type d -name "*.${_profile}")
 	
-	mv -- /home/user/user.js "${_pname}/"
+	cp /home/user/.local/src/ff/user.js "${_pname}/"
+	cp -r /home/user/.local/src/ff/extensions "${_pname}/"
 
 	printf '%s\n' "Will launch firefox in 5 seconds. This will set the default profile."
 	sleep 5
 	/home/user/.local/bin/ff-release -P "${_profile}" &
+}
+
+upd_path() {
+	printf '%s\n' "Updating dmenu path..."
+	[[ -f /home/user/.cache/demnu_run ]] && rm /home/user/.cache/dmenu_run
+	dmenu_path
 }
 
 mkdir -p /home/user/{.firefox,Downloads}
@@ -40,7 +47,6 @@ tar -xvf Downloads/"${NAME}" -C /home/user/.firefox --strip-components=1
 printf '%s\n' "Creating symlink..."
 mkdir -p /home/user/.local/bin
 ln -sf /home/user/.firefox/firefox /home/user/.local/bin/ff-release
-printf '%s\n' "Updating dmenu's path..."
-[[ -f /home/user/.cache/dmenu_run ]] && rm /home/user/.cache/dmenu_run
-[[ $USERJS == yes ]] && create_profile
+upd_path
+[[ $USERSETUP == yes ]] && create_profile
 printf '%s\n' "Done"
