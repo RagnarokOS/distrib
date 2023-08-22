a1fcb44d4549062911c9308339cf370266cde1ca
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2023-08-22 19:40:38 -0400

    Added web search capabilities

--- live/config/includes.chroot_after_packages/etc/skel/.local/bin/bro.sh
+++ live/config/includes.chroot_after_packages/etc/skel/.local/bin/bro.sh
@@ -7,9 +7,53 @@
 # Can open multiple URLs at once. The '-bm' option will display bookmarks
 # in dmenu and open the selected one.
 
+usage() {
+	printf '%s\n' "
+bro.sh: a wrapper script around Suckless' surf. When used with no option,
+the script will launch one or more URL in a surf window with both javascript
+and cookies disabled (they can be toggled on as needed via keybindings).
+
+For details on surf usage and keyboard shortcuts, refer to its manual page.
+
+Usage examples:
+
+Single URL:		bro.sh https://surf.suckless.org
+Open two URLs:		bro.sh https://surf.suckless.org https://openbsd.org
+
+Web Search:
+
+bro.sh !b term		search for term with brave search
+bro.sh !ddg term	search for term with duck duck go
+bro.sh !g term		search for term with google search
+
+Opening bookmarks:
+
+Using bro.sh -bm, all URLs listed in ~/.surf/bookmarks.txt will be displayed
+in dmenu, and surf will open the selected URL. The bro.sh -bm command is tied
+to the alt + s keyboard shortcut in sxhkdrc.
+"
+}
+
 bm() {
 	surf_cmd "$(while IFS= read -r line; do printf '%s\n' "$line"; done \
 		< "$HOME"/.surf/bookmarks.txt | dmenu -p open: -l 15 | cut -f2 -d ' ')"
+}
+
+wsearch() {
+	local _engine=$1 _search=$2 _url
+
+	case "${_engine}" in
+		!b)	_url="https://search.brave.com/search?q=${_search}&source=web"
+			;;
+		!ddg)	_url="https://duckduckgo.com/?q=${_search}"
+			;;
+		!g)	_url="https://www.google.com/search?q=${_search}"
+			;;
+		*)	_url="https://search.brave.com/search?q=${_search}&source=web"
+			;;
+	esac
+
+	surf_cmd "${_url}"
 }
 
 surf_cmd() {
@@ -22,6 +66,14 @@ surf_cmd() {
 }
 
 case "$1" in
+	!b)	shift
+		surf_cmd "https://search.brave.com/search?q=$(echo "$@" | tr " " "+")&source=web"
+		;;
+	!ddg)	shift
+		surf_cmd "https://duckduckgo.com/?q=$(echo "$@" | tr " " "+")"
+		;;
+	!g)	surf_cmd "https://google.com/search?1=$(echo "$@" | tr " " "+")"
+		;;
 	-bm)	bm
 		;;
 	*)	surf_cmd "$@"
