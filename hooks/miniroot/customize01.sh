037d4ceb815c52ab8299f30eb38c9556b85295d5
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2023-10-02 11:50:33 -0400

    set up _sysupdate user

--- hooks/miniroot/customize01.sh
+++ hooks/miniroot/customize01.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# $Ragnarok: customize01.sh,v 1.1 2023/10/02 15:47:56 lecorbeau Exp $
+# $Ragnarok: customize01.sh,v 1.2 2023/10/02 15:50:29 lecorbeau Exp $
 
 set -e
 
@@ -21,11 +21,13 @@ chroot "$1" addgroup --system wheel
 # Making sure root's interactive shell is ksh
 sed -i 's/bash/ksh/g' "$1"/etc/passwd
 
-# Symlinking signify-openbsd to signify
-chroot "$1" ln -sf /usr/bin/signify-openbsd /usr/bin/signify
-
 # Set the default DEBIAN_FRONTEND to 'Readline'
 chroot "$1" echo 'debconf debconf/frontend select Readline' | debconf-set-selections
+
+# Set up the _sysupdate user
+chroot "$1" useradd --system --no-create-home --home /nonexistent --shell=/usr/sbin/nologin _sysupdate
+chroot "$1" chown _sysupdate:_sysupdate /var/db/updates/sysupdate.list
+
 
 # Needed to make the rootfs reproducible
 rm "$1"/etc/resolv.conf
