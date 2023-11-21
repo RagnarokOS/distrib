e4ebd7e1b7c53b2d31decf4d31083a9113011f48
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2023-11-21 11:41:56 -0500

    handle dummies in base hook

--- hooks/base/customize01.sh
+++ hooks/base/customize01.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Customize script
-# $Ragnarok: customize01.sh,v 1.3 2023/10/17 15:19:55 lecorbeau Exp $
+# $Ragnarok: customize01.sh,v 1.4 2023/11/21 16:41:35 lecorbeau Exp $
 
 set -e
 
@@ -10,12 +10,14 @@ chroot "$1" useradd --system --no-create-home --home /nonexistent --shell=/usr/s
 
 # Copy then install dummy packages
 mkdir -p "$1"/usr/src/ragnarok
-cp dummies/* "$1"/usr/src/ragnarok/
+cp dummies/*.deb "$1"/usr/src/ragnarok/
 for _file in base-files.conffiles base-files.list base-files.md5sums base-files.postinst; do
 	rm "$1"/var/lib/dpkg/info/"$_file"
 done
-chroot "$1" dpkg -i /usr/src/ragnarok/base-files_99+ragnarok01_amd64.deb
-chroot "$1" dpkg -i /usr/src/ragnarok/ed_99+ragnarok01_amd64.deb
+for _file in sed.list sed.md5sums; do
+	rm "$1"/var/lib/dpkg/info/"$_file"
+done
+chroot "$1" for _pkg in /usr/src/ragnarok/*.deb; do dpkg -i "$_pkg"; done
 
 # Enable the wheel group
 sed -i '15 s/^# //' "$1"/etc/pam.d/su
