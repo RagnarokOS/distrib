2eeef9b39d2a5dbf6bab6a89a6a73abfaa72b939
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2023-12-11 11:12:19 -0500

    bump kernel versions

--- live/live.mk
+++ live/live.mk
@@ -1,4 +1,4 @@
-# $Ragnarok: live.mk,v 1.3 2023/12/09 19:45:45 lecorbeau Exp $
+# $Ragnarok: live.mk,v 1.4 2023/12/11 16:12:14 lecorbeau Exp $
 
 # iso-specific config file
 
@@ -11,5 +11,5 @@ LIVE_BOOTPARAMS	= live-config.hooks=filesystem live-config.locales=en_CA.UTF-8 \
 		  init_on_alloc=1 init_on_free=1 page_alloc.shuffle=1 pti=on \
 		  randomize_kstack_offset=on vsyscall=none debugfs=off \
 		  lockdown=confidentiality
-KERNEL		= 6.1.64
-DEBKERNEL	= 6.1.0-14
+KERNEL		= 6.1.66
+DEBKERNEL	= 6.1.0-15
