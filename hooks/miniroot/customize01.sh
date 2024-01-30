c0317b6d7d59c56ec68bcb3b55f49cc558a97dfb
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2024-01-30 13:33:32 -0500

    base-files is now a package

--- hooks/miniroot/customize01.sh
+++ hooks/miniroot/customize01.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# $Ragnarok: customize01.sh,v 1.10 2023/11/06 17:59:42 lecorbeau Exp $
+# $Ragnarok: customize01.sh,v 1.11 2024/01/30 18:33:25 lecorbeau Exp $
 
 set -e
 
@@ -10,25 +10,11 @@ chroot "$1" useradd --system --no-create-home --home /nonexistent --shell=/usr/s
 # Copy then install dummy packages
 mkdir -p "$1"/usr/src/ragnarok
 cp dummies/* "$1"/usr/src/ragnarok/
-for _file in base-files.conffiles base-files.list base-files.md5sums base-files.postinst; do
-	rm "$1"/var/lib/dpkg/info/"$_file"
-done
-chroot "$1" dpkg -i /usr/src/ragnarok/base-files_999+ragnarok01_amd64.deb
 chroot "$1" dpkg -i /usr/src/ragnarok/ed_999+ragnarok01_amd64.deb
 
 # Enable the wheel group
 sed -i '15 s/^# //' "$1"/etc/pam.d/su
 chroot "$1" addgroup --system wheel
 
-# Add ksh to /etc/shells
-chroot "$1" add-shell /bin/ksh
-
 # Making sure root's interactive shell is ksh
 sed -i 's/bash/ksh/g' "$1"/etc/passwd
-
-# Set the default DEBIAN_FRONTEND to 'Readline'
-chroot "$1" echo 'debconf debconf/frontend select Readline' | debconf-set-selections
-
-# Fix the symlinks
-chroot "$1" ln -sf /etc/dpkg/origins/ragnarok /etc/dpkg/origins/default
-chroot "$1" ln -sf /usr/lib/os-release /etc/os-release
