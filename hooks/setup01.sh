4a4f3c84c1554da9867a6136a7e67d20dcd44123
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2024-05-09 11:27:24 -0400

    setup01.sh: copy kernupd tool to miniroot

--- hooks/setup01.sh
+++ hooks/setup01.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Customize before the setup phase
-# $Ragnarok: setup01.sh,v 1.5 2024/05/08 15:07:10 lecorbeau Exp $
+# $Ragnarok: setup01.sh,v 1.6 2024/05/09 15:27:15 lecorbeau Exp $
 
 set -e
 
@@ -10,6 +10,7 @@ mkdir -p "$1"/etc
 cp -r ../src/base/etc/apt/ "$1"/etc/
 cp -r ../src/base/etc/dpkg "$1"/etc/
 cp -r ../src/base/etc/signify "$1"/etc/
+cp ../src/base/usr/bin/kernupd "$1"/usr/bin/
 
 # Creating /etc/mailname. bsd-mailx and dma are installed non-interactively and we need
 # this file to prevent dpkg from setting mailname to 'root' when dma is installed.
