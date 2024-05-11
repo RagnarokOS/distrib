7087ed4991ae338e40aff571de7a6b7c415feffa
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2024-05-11 11:40:42 -0400

    boot: disable the binfmt-support init script

--- iso/live.mk
+++ iso/live.mk
@@ -1,4 +1,4 @@
-# $Ragnarok: live.mk,v 1.9 2024/05/07 17:09:42 lecorbeau Exp $
+# $Ragnarok: live.mk,v 1.10 2024/05/11 15:40:30 lecorbeau Exp $
 
 # iso-specific config file
 
@@ -7,7 +7,7 @@ ISO_MODE	= live
 HDD_LABEL	= RAGNAROK
 LIVE_BOOTPARAMS	= live-config.hooks=filesystem live-config.locales=en_CA.UTF-8 \
 		  live-config.timezone=Canada/Eastern live-config.hostname=ragnarok \
-		  live-config.noautologin live-config.sysv-rc=openntpd slab_nomerge \
+		  live-config.noautologin live-config.sysv-rc=binfmt-support slab_nomerge \
 		  init_on_alloc=1 init_on_free=1 page_alloc.shuffle=1 pti=on \
 		  randomize_kstack_offset=on vsyscall=none debugfs=off \
 		  lockdown=confidentiality
