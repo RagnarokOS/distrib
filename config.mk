ed1f26d6f70ab374627b2f1bf78cc678d5e2acd2
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2024-05-29 11:13:08 -0400

    config.mk: add ed and wget to PACKAGES

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # General config file for releases - Work In Progress
-# $Ragnarok: config.mk,v 1.13 2024/05/28 15:36:26 lecorbeau Exp $
+# $Ragnarok: config.mk,v 1.14 2024/05/29 15:13:04 lecorbeau Exp $
 
 DISTRO		= ragnarok
 PRETTY_NAME	= Ragnarok
@@ -13,4 +13,4 @@ COMPONENTS	= main non-free-firmware
 
 # Packages included in everything.
 PACKAGES	= policy-rcd-declarative-deny-all usrmerge ca-certificates \
-		  oksh signify-openbsd less
+		  oksh signify-openbsd less wget ed
