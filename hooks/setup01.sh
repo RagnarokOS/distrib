60695885407290b256793db1766307ab71b97bc7
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2024-05-29 11:11:54 -0400

    hooks/setup01.sh: copy shlib

--- hooks/setup01.sh
+++ hooks/setup01.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Customize before the setup phase
-# $Ragnarok: setup01.sh,v 1.7 2024/05/09 15:32:26 lecorbeau Exp $
+# $Ragnarok: setup01.sh,v 1.8 2024/05/29 15:11:36 lecorbeau Exp $
 
 set -e
 
@@ -12,6 +12,7 @@ cp -r ../src/base/etc/apt/ "$1"/etc/
 cp -r ../src/base/etc/dpkg "$1"/etc/
 cp -r ../src/base/etc/signify "$1"/etc/
 cp ../src/base/usr/bin/kernupd "$1"/usr/bin/
+cp ../src/base/usr/lib/ragnarok-shlib "$1"/usr/lib/
 
 # Creating /etc/mailname. bsd-mailx and dma are installed non-interactively and we need
 # this file to prevent dpkg from setting mailname to 'root' when dma is installed.
