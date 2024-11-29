9f88a6474883870612c1b15f7e89b8baf3d1aec3
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2024-11-29 11:05:41 -0500

    update setup01.sh hook

--- hooks/miniroot/setup01.sh
+++ hooks/miniroot/setup01.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Customize before the setup phase
-# $Ragnarok: setup01.sh,v 1.10 2024/05/29 15:24:09 lecorbeau Exp $
+# $Ragnarok: setup01.sh,v 2.1 2024/11/29 16:05:19 lecorbeau Exp $
 
 set -e
 
@@ -12,8 +12,8 @@ mkdir -p "$1"/usr/lib
 cp -r ../src/base/etc/apt/ "$1"/etc/
 cp -r ../src/base/etc/dpkg "$1"/etc/
 cp -r ../src/base/etc/signify "$1"/etc/
-cp ../src/base/usr/bin/kernupd "$1"/usr/bin/
-chmod 755 "$1"/usr/bin/kernupd
+cp ../src/base/usr/share/ragnarok/keyrings/brave-browser-archive-keyring.gpg "$1"/usr/share/keyrings/
+chmod 644 /usr/share/keyrings/brave-browser-archive-keyring.gpg
 cp ../src/base/usr/lib/ragnarok-shlib "$1"/usr/lib/
 
 # Creating /etc/mailname. bsd-mailx and dma are installed non-interactively and we need
