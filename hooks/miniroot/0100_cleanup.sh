9f9443fa97a437381cdd6a40412500dfc7cd497f
Ian LeCorbeau <I-LeCorbeau@protonmail.com>
2025-05-26 13:23:55 -0400

    0100_cleanup.sh: rm more stuff

--- hooks/miniroot/0100_cleanup.sh
+++ hooks/miniroot/0100_cleanup.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
-# $Ragnarok: 0100_cleanup.sh,v 1.1 2025/05/26 15:49:22 lecorbeau Exp $
+# $Ragnarok: 0100_cleanup.sh,v 1.2 2025/05/26 17:23:51 lecorbeau Exp $
 #
 # Cleanup miniroot before creating tarball.
+
+set -e
 
 TARGET="$(getval DESTDIR config.mk)/miniroot"
 
 # Start by removing resolv.conf, if it exists.
-if [ -f "${TARGET}/etc/resilv.conf" ]; then
+if [ -f "${TARGET}/etc/resolv.conf" ]; then
 	rm -f "${TARGET}/etc/resolv.conf"
 fi
 
+# rm root's .bash_history
+rm "${TARGET}/root/.bash_history"
 
+# Cleanup /var
+for _dir in lock log run tmp cache/distfiles; do
+	rm -rf "${TARGET}/var/${_dir}/*"
+done
+rm -rf "${TARGET}/var/db/repos/*/*"
