851891336ff6145bb22939dea78cb3dc325fec6c
Ian LeCorbeau <I-LeCorbeau@protonmail.com>
2025-06-17 11:11:22 -0400

    Only extract the toolchain if desired
    Some may not care about using the pre-built LLVM toolchain, or using
    their own pre-built toolchain.

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # General config file for releases - Work In Progress
-# $Ragnarok: config.mk,v 2.6 2025/05/27 16:12:26 lecorbeau Exp $
+# $Ragnarok: config.mk,v 2.7 2025/06/17 15:10:35 lecorbeau Exp $
 
 # Location of the distrib repo. The default is '/usr/src/ragnarok/distrib'
 # but can be set to anything else (eg. .local/src/distrib). This allows
@@ -11,6 +11,9 @@ VERSION		= 02
 
 # The Gentoo tarball used as a base
 TARBALL		= 
+
+# Should a toolchain tarball be fetched/extracted? 'true' or 'false'.
+TOOLCHAIN	= true
 
 # The top directory where the chroot will be created
 DESTDIR		= ${SRCDIR}
--- hooks/miniroot/0015_setup.sh
+++ hooks/miniroot/0015_setup.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-# $Ragnarok: 0015_setup.sh,v 1.8 2025/05/27 16:17:50 lecorbeau Exp $
+# $Ragnarok: 0015_setup.sh,v 1.10 2025/06/17 15:11:09 lecorbeau Exp $
 #
 # Extract and install the toolchain tarball to the chroot.
 
 TARGET="$(getval MINIROOT config.mk)"
 
-/usr/bin/tar xpvf toolchain.tgz -C "${TARGET}/var/cache"
-./chrootcmd "${TARGET}" "PKGDIR=/var/cache/toolchain emerge -v --usepkg @llvm-toolchain"
-./chrootcmd "${TARGET}" "PKGDIR=/var/cache/toolchain emerge -v --usepkg binutils gcc glibc"
-./chrootcmd "${TARGET}" "PKGDIR=/var/cache/toolchain emerge -v --usepkg @build"
+# Extract the toolchain if it is set to 'yes' in config.mk
+if [ "$(getval TOOLCHAIN config.mk)" = "true" ]; then
+	/usr/bin/tar xpvf toolchain.tgz -C "${TARGET}/var/cache"
+	./chrootcmd "${TARGET}" "PKGDIR=/var/cache/toolchain emerge -v --usepkg @llvm-toolchain"
+	./chrootcmd "${TARGET}" "PKGDIR=/var/cache/toolchain emerge -v --usepkg binutils gcc glibc"
+	./chrootcmd "${TARGET}" "PKGDIR=/var/cache/toolchain emerge -v --usepkg @build"
+fi
