6b66c38b1084c36669f8cd40a686a4f764336c7a
Ian LeCorbeau <I-LeCorbeau@protonmail.com>
2025-06-27 12:12:28 -0400

    hooks/miniroot/0015_setup.sh switch to arch-chroot

--- hooks/miniroot/0015_setup.sh
+++ hooks/miniroot/0015_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# $Ragnarok: 0015_setup.sh,v 1.10 2025/06/17 15:11:09 lecorbeau Exp $
+# $Ragnarok: 0015_setup.sh,v 1.11 2025/06/27 16:12:11 lecorbeau Exp $
 #
 # Extract and install the toolchain tarball to the chroot.
 
@@ -9,7 +9,7 @@ TARGET="$(getval MINIROOT config.mk)"
 # Extract the toolchain if it is set to 'yes' in config.mk
 if [ "$(getval TOOLCHAIN config.mk)" = "true" ]; then
 	/usr/bin/tar xpvf toolchain.tgz -C "${TARGET}/var/cache"
-	./chrootcmd "${TARGET}" "PKGDIR=/var/cache/toolchain emerge -v --usepkg @llvm-toolchain"
-	./chrootcmd "${TARGET}" "PKGDIR=/var/cache/toolchain emerge -v --usepkg binutils gcc glibc"
-	./chrootcmd "${TARGET}" "PKGDIR=/var/cache/toolchain emerge -v --usepkg @build"
+	arch-chroot "${TARGET}" /bin/bash "PKGDIR=/var/cache/toolchain emerge -v --usepkg @llvm-toolchain"
+	arch-chroot "${TARGET}" /bin/bash "PKGDIR=/var/cache/toolchain emerge -v --usepkg binutils gcc glibc"
+	arch-chroot "${TARGET}" /bin/bash "PKGDIR=/var/cache/toolchain emerge -v --usepkg @build"
 fi
