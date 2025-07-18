8dc5449ee3041c36f1447f420885a748e5dba326
Ian LeCorbeau <I-LeCorbeau@protonmail.com>
2025-07-18 12:20:57 -0400

    miniroot/0015_setup.sh: return to chrootcmd
    see: https://github.com/RagnarokOS/distrib/commit/79e458da9a69735aa72df7fbf3d3fceead1f701f

--- hooks/miniroot/0015_setup.sh
+++ hooks/miniroot/0015_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# $Ragnarok: 0015_setup.sh,v 1.11 2025/06/27 16:12:11 lecorbeau Exp $
+# $Ragnarok: 0015_setup.sh,v 1.12 2025/07/18 16:19:44 lecorbeau Exp $
 #
 # Extract and install the toolchain tarball to the chroot.
 
@@ -9,7 +9,7 @@ TARGET="$(getval MINIROOT config.mk)"
 # Extract the toolchain if it is set to 'yes' in config.mk
 if [ "$(getval TOOLCHAIN config.mk)" = "true" ]; then
 	/usr/bin/tar xpvf toolchain.tgz -C "${TARGET}/var/cache"
-	arch-chroot "${TARGET}" /bin/bash "PKGDIR=/var/cache/toolchain emerge -v --usepkg @llvm-toolchain"
-	arch-chroot "${TARGET}" /bin/bash "PKGDIR=/var/cache/toolchain emerge -v --usepkg binutils gcc glibc"
-	arch-chroot "${TARGET}" /bin/bash "PKGDIR=/var/cache/toolchain emerge -v --usepkg @build"
+	./chrootcmd "${TARGET}" "PKGDIR=/var/cache/toolchain emerge -v --usepkg @llvm-toolchain"
+	./chrootcmd "${TARGET}" "PKGDIR=/var/cache/toolchain emerge -v --usepkg binutils gcc glibc"
+	./chrootcmd "${TARGET}" "PKGDIR=/var/cache/toolchain emerge -v --usepkg @build"
 fi
